ea0741807d3d1783fae72cb471243a8171afbe25
stgodjah <godjah@ganj.site>
2018-10-07 16:06:56 +0300

    Getting android id

--- app/src/main/java/com/btcrelax/bitganjshop/MainActivity.java
+++ app/src/main/java/com/btcrelax/bitganjshop/MainActivity.java
@@ -2,6 +2,13 @@ package com.btcrelax.bitganjshop;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.view.*;
+import android.security.*;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.provider.Settings.Secure;
+
 
 public class MainActivity extends Activity 
 {
@@ -11,4 +18,40 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate main_menu.xml 
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.menuAbout:
+				String vId = "Device id:" + getUniqueID();
+				Toast.makeText(this, vId, Toast.LENGTH_SHORT).show();
+				return true;
+			case R.id.menuExit:
+				finish();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public String getUniqueID(){    
+		String myAndroidDeviceId = "";
+		TelephonyManager mTelephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		if (mTelephony.getDeviceId() != null){
+			myAndroidDeviceId = mTelephony.getDeviceId(); 
+		}else{
+			myAndroidDeviceId = Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID); 
+		}
+		return myAndroidDeviceId;
+	}
 }
